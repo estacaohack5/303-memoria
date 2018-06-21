d7d5026e76b6ad65cd5d83bffe743b0ae0f2c2ee
Renan Rodrigo <renanrb@ime.usp.br>
2018-06-21 11:29:10 -0300

    Escrever logica do jogo

--- script.js
+++ script.js
@@ -4,17 +4,54 @@ const imagens = embaralhar(duplicar(personagens));
 
 const cartas = document.querySelectorAll(".item");
 
-function jogar(){
-    if(this.classList.contains("active")){
-        this.classList.remove("active");
-        this.style.backgroundImage = "url('imgs/logo.png')"
+let jogando = false;
+
+function desativar(carta){
+    carta.classList.remove("ativo");
+    carta.style.backgroundImage = "url('imgs/logo.png')"
+    carta.onclick = clicar;
+}
+
+function ativar(carta){
+    carta.classList.add("ativo");
+    carta.style.backgroundImage = `url('imgs/${imagens[Number(carta.id)]}.png')`;
+    carta.onclick = null;
+}
+
+function verificar(){
+    let ativos = document.querySelectorAll(".ativo");
+    if(ativos[0].style.backgroundImage === ativos[1].style.backgroundImage){
+        for(let carta of ativos){
+            carta.classList.remove("ativo");
+        }
     }
     else{
-        this.classList.add("active");
-        this.style.backgroundImage = `url('imgs/${imagens[Number(this.id)]}.png')`;
+        for(let carta of ativos){
+            desativar(carta);
+        }
+    }
+}
+
+function clicar(){
+    
+    if(jogando){
+        jogando = false;
+    }
+    else{
+        jogando = true;
+    }
+    
+    if(this.classList.contains("ativo")){
+        desativar(this);
+    }
+    else{
+        ativar(this);
+    }
+    if(!jogando){
+        setTimeout(verificar, 1000);
     }
 }
 
 for (let carta of cartas){
-    carta.onclick = jogar;
+    carta.onclick = clicar;
 }
